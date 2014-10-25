1e623c5d1807c5ad9fdad2d4706f54b9ff866f6b
Khilo84 <conanedo08@gmail.com>
2014-10-25 07:18:40 -0500

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -6,7 +6,6 @@ while ((seq[i] + seq[i-1]) <= 4000000):
 	seq.append(seq[i] + seq[i-1])
 	i = i + 1
 	
-print('Value of i = '), i	
 print ('Fib. Series(100) = '), seq
 for k in range(i+1):
 	if (seq[k] % 2 == 0):
