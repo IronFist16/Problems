73a08a3f2f5b7928438d096d14611eebe7bb39da
Khilo84 <conanedo08@gmail.com>
2014-10-25 07:25:01 -0500

    Update Problem1.py
    More efficient coding, by Assem guidance

--- Problem1.py
+++ Problem1.py
@@ -1,14 +1,3 @@
 
 
-i = 0
-l = 0
-
-while (i<1000):
-	if ((i % 3) == 0 or (i % 5) == 0) :
-		l = l + i
-		print 'i = ' + str(i) + '\n'
-		print 'Sum = ' + str(l) + '\n'
-	i = i+1
-		
-print 'Total of 3 and 5 multipes is = ' + str(l)
-	
+print 'Total of 3 and 5 multipes is = ', sum(set(xrange(0,1000,3))|set(xrange(0,1000,5)))
