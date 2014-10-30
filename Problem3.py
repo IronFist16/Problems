bf9503445a2f6be866618102e9a3c6af4a9b5e84
Assem Chelli <assem.ch@gmail.com>
2014-10-31 00:12:01 +0100

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -2,8 +2,9 @@ prime = [2,3,5,7]
 
 myNum = input('Please enter Value = ')
 	
-x = 1
-while (x < myNum):
-	if(all((x % y) != 0 for y in prime) and (myNum % x) == 0):
-		print x
-	x = x+1
+while (x*x < myNum):
+    while(myNum % x == 0):
+        myNum /= x 
+    x+=1
+
+print myNum
