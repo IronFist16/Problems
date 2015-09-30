104affbad39c03abdd3abe71afcc379b43f05a5d
Khalil Ammour <khalil.ammour@gmail.com>
2015-09-30 17:18:38 +0400

    Create Problem15.py

---
+++ Problem15.py
@@ -0,0 +1,12 @@
+def fct(size):
+	d={}
+	for i in range(size):
+		for j in range(size):
+			d[(i,j)] = 0
+			if i == 0 or j == 0:
+				d[(i,j)] = 1
+			else:
+				d[(i,j)] = d[(i-1),j] + d[(i,j-1)]
+	return d[(size-1,size-1)]
+	
+print fct(20)
